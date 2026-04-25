f80ab8737f9d8b751e5abd87ca54d8029a0426ce
SolitaryRehman <ariaz.bscs25seecs@seecs.edu.pk>
2026-04-25 18:55:54 +0500

    added alert manager class methods

--- cyber/service/AlertManager.java
+++ cyber/service/AlertManager.java
@@ -1,10 +1,48 @@
 package service;
 
 import entities.Alert;
+import entities.Log;
+import entities.enums.AlertType;
 
+import java.time.LocalDateTime;
 import java.util.Vector;
 
 public class AlertManager
 {
     private Vector<Alert> alerts;
+
+    public AlertManager()
+    {
+        this.alerts = new Vector<>();
+    }
+
+    public Vector<Alert> getAlerts()
+    {
+        return alerts;
+    }
+
+    public void setAlerts(Vector<Alert> alerts)
+    {
+        this.alerts = alerts;
+    }
+
+    public Alert createAlert(Log log, AlertType type)
+    {
+        Alert alert = new Alert(log, type, LocalDateTime.now());
+        alerts.add(alert);
+        return alert;
+    }
+
+    public Vector<Alert> getAlertsByLevel(AlertType type)
+    {
+        Vector<Alert> filtered = new Vector<>();
+        for (Alert alert : alerts)
+        {
+            if (alert.getType() == type)
+            {
+                filtered.add(alert);
+            }
+        }
+        return filtered;
+    }
 }
